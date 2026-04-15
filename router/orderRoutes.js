89ef8ec9741f85132c6f01db24388076a2a56a49
YuvrajS2767 <yuvrajsiwal14@gmail.com>
2026-04-15 19:21:16 +0530

    fixed the API calling

--- router/orderRoutes.js
+++ router/orderRoutes.js
@@ -15,7 +15,7 @@ import {
 const router = express.Router();
 router.post("/new", isAuthenticated, placeNewOrder);
 router.get("/:orderId", isAuthenticated, fetchSingleOrder);
-router.get("/orders/me", isAuthenticated, fetchMyOrders);
+router.get("/ordersme", isAuthenticated, fetchMyOrders);
 router.get(
   "/admin/getall",
   isAuthenticated,
