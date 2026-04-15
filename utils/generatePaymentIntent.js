36699dd3f2e75423f109b6ad723bb241a9efcb02
YuvrajS2767 <yuvrajsiwal14@example.com>
2026-04-15 08:12:43 +0530

    fixed the currency issue

--- utils/generatePaymentIntent.js
+++ utils/generatePaymentIntent.js
@@ -2,23 +2,27 @@ import database from "../database/db.js";
 import Stripe from "stripe";
 import { config } from "dotenv";
 config();
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function generatePaymentIntent(orderId, totalPrice) {
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalPrice * 100,
-      currency: "usd",
+      amount: totalPrice * 100, // 
+      currency: "inr",          // 
+      automatic_payment_methods: {
+        enabled: true,          // 
+      },
     });
 
     await database.query(
       "INSERT INTO payments (order_id, payment_type, payment_status, payment_intent_id) VALUES ($1, $2, $3, $4) RETURNING *",
-      [orderId, "Online", "Pending", paymentIntent.client_secret]
+      [orderId, "Online", "Pending", paymentIntent.id] //
     );
 
     return { success: true, clientSecret: paymentIntent.client_secret };
   } catch (error) {
-    console.error("Payment Error:", error.message || error);
-    return { success: false, message: "Payment Failed." };
+    console.error("Payment Error:", error);
+    return { success: false, message: error.message || "Payment Failed." };
   }
 }
