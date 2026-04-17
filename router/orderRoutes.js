636e8735cb4e922001a0bfc63f6165a339452c62
YuvrajS2767 <yuvrajsiwal14@example.com>
2026-04-17 18:21:35 +0530

    FIXED ORDER

--- router/orderRoutes.js
+++ router/orderRoutes.js
@@ -13,21 +13,28 @@ import {
 } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+
 router.post("/new", isAuthenticated, placeNewOrder);
-router.get("/:orderId", isAuthenticated, fetchSingleOrder);
+
+// ✅ FIXED ORDER (VERY IMPORTANT)
 router.get("/orders/me", isAuthenticated, fetchMyOrders);
+
+router.get("/:orderId", isAuthenticated, fetchSingleOrder);
+
 router.get(
   "/admin/getall",
   isAuthenticated,
   authorizedRoles("Admin"),
   fetchAllOrders
 );
+
 router.put(
   "/admin/update/:orderId",
   isAuthenticated,
   authorizedRoles("Admin"),
   updateOrderStatus
 );
+
 router.delete(
   "/admin/delete/:orderId",
   isAuthenticated,
